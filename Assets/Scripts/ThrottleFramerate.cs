0374fbfc90f1de5cde00a5427e6829f6c3128aea
Efraim Kent <efraim.kent@student.forsbergsskola.se>
2021-09-15 10:37:30 +0200

    Added framrate throttle script

---
+++ Assets/Scripts/ThrottleFramerate.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrottleFramerate : MonoBehaviour
+{
+    public int targetFramerate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = this.targetFramerate;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
